36287b5571667af3098995f57fff880992769e2d
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-06-22 16:29:48 +0300

    feat(apps): create `cars` app

---
+++ apps/cars/admin.py
@@ -0,0 +1,70 @@
+from django.contrib import admin
+
+from .models import Car, CarFeature, CarImage
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "make",
+        "model",
+        "year",
+        "price",
+        "created",
+        "modified",
+    )
+    list_filter = (
+        "model",
+        "price",
+        "year",
+        "created",
+        "modified",
+    )
+    search_fields = (
+        "make",
+        "model",
+        "year",
+        "price",
+    )
+    list_editable = ("price",)
+    list_per_page = 25
+
+
+@admin.register(CarFeature)
+class CarFeatureAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "created",
+        "modified",
+    )
+    list_filter = (
+        "name",
+        "created",
+        "modified",
+    )
+    search_fields = ("name",)
+    list_per_page = 25
+
+
+@admin.register(CarImage)
+class CarImageAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "car",
+        "user",
+        "created",
+        "modified",
+    )
+    list_filter = (
+        "car",
+        "user",
+        "created",
+        "modified",
+    )
+    search_fields = (
+        "car",
+        "user",
+    )
+    list_per_page = 25
---
+++ apps/cars/views.py
@@ -0,0 +1,51 @@
+from rest_framework import status
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Car, CarFeature, CarImage
+from .serializers import CarFeatureSerializer, CarImageSerializer, CarSerializer
+
+
+class CarViewSet(ModelViewSet):
+    """
+    Car ViewSet
+    """
+
+    queryset = Car.objects.prefetch_related("features").all()
+    serializer_class = CarSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+    filterset_fields = ("make", "model", "year", "price", "mileage", "engine_size")
+    search_fields = ("make", "model", "year", "price", "mileage", "engine_size")
+    ordering_fields = ("year", "price", "mileage", "engine_size")
+
+
+class CarImageViewSet(ModelViewSet):
+    """
+    Car Image ViewSet
+    """
+
+    queryset = CarImage.objects.all()
+    serializer_class = CarImageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        # Check if the user is the owner of the image
+        car_image = self.get_object()
+        if car_image.user != request.user:
+            return Response(
+                {"detail": "You do not have permission to delete this image."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        return super().destroy(request, *args, **kwargs)
+
+
+class CarFeatureViewSet(ModelViewSet):
+    """
+    Car Feature ViewSet
+    """
+
+    queryset = CarFeature.objects.all()
+    serializer_class = CarFeatureSerializer
