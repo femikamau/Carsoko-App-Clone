64a420cc3092784e5197e7393346757729e760df
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-06-26 17:17:44 +0300

    refactor(apps): update `UserViewSet` inheritance

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,19 +1,11 @@
-from rest_framework.mixins import (
-    ListModelMixin,
-    RetrieveModelMixin,
-)
 from rest_framework.permissions import IsAdminUser
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import User
 from .serializers import UserSerializer
 
 
-class UserViewSet(
-    RetrieveModelMixin,
-    ListModelMixin,
-    GenericViewSet,
-):
+class UserViewSet(ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAdminUser,)
