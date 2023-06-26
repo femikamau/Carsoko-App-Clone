58947e4ff822298e7be6515947eaaa9fad210789
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-06-26 17:10:09 +0300

    refactor(apps):update user model serializer fields

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -4,11 +4,19 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="user-detail")
+
+    pictures = serializers.HyperlinkedRelatedField(
+        view_name="image-detail", read_only=True, many=True, source="images"
+    )
+
     class Meta:
         model = User
         fields = (
             "id",
+            "url",
             "email",
             "first_name",
             "last_name",
+            "pictures",
         )
