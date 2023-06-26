9d36cf7c9cc46dbcdf25b876718ab11c7c1f19a2
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-06-26 17:11:01 +0300

    refactor(project): update apis endpoint route

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path(route="admin/", view=admin.site.urls),
-    path(route="apis/", view=include("apis.urls")),
+    path(route="api/", view=include("apis.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
